edba386cb16ddf1b6debe49cebc7c94e2f55b708
petko619 <147520606+petko619@users.noreply.github.com>
2024-01-29 07:32:34 +0200

    Add files via upload

---
+++ Нова папка/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Въведете числа, разделени с точка:");
+string inputNumbers = Console.ReadLine();
+
+List<double> numbers = inputNumbers.Split('.').Select(double.Parse).ToList();
+
+double average = numbers.Average();
+double mode = numbers.GroupBy(n => n).OrderByDescending(g => g.Count()).Select(g => g.Key).FirstOrDefault();
+double median = numbers.OrderBy(n => n).ElementAt(numbers.Count / 2);
+
+Console.WriteLine($"Средно аритметично: {average}");
+Console.WriteLine($"Мода: {mode}");
+Console.WriteLine($"Медиана: {median}");
